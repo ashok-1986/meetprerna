ea12174327782caad3182e8395f65cb61f59c720
Ashok Verma <verma.86ashok@gmail.com>
2026-08-09 21:14:27 +0530

    Fix missing gsap import in PageWrapper

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -2,6 +2,7 @@
 
 import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { createStaggeredReveal, createDoubleExposureTransition } from "@/lib/animations/factories";
 
 export default function PageWrapper({ children }: { children: React.ReactNode }) {
